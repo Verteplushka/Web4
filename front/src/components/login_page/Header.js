b1044edc68c42d252f97645727609955a3ebf159
Verteplushka <kolbacin_vlad@mail.ru>
2024-01-19 16:10:48 +0300

    Styles experiments

--- front/src/components/login_page/Header.js
+++ front/src/components/login_page/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
-const Header = ({name, group, variant}) => {
+const Header = ({ name, group, variant }) => {
   return (
-    <header >
-        <h1>{name}, {group},  Вариант №{variant}</h1>
+    <header>
+      <nav className="navbar navbar-dark bg-dark">
+        <h1 class="navbar-brand">
+          {name}, {group}, Вариант №{variant}
+        </h1>
+      </nav>
     </header>
-  )
+  );
 };
 
 export default Header;
--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
-import './LoginPage.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+// import './LoginPage.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
