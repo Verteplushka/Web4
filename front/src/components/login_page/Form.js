4851cbb5f4d62c35d5a195ed9fb10d255a11b00d
Verteplushka <kolbacin_vlad@mail.ru>
2024-01-19 15:52:53 +0300

    Resolved small issues in login_page

--- front/src/components/login_page/Form.js
+++ front/src/components/login_page/Form.js
@@ -1,21 +1,35 @@
-import React from 'react';
+import React from "react";
 
 const Form = () => {
-    return (
-      <form>
-        <div class="form-group">
-          <label for="login">Login</label>
-          <input type="text" class="form-control" id="login" placeholder='enter your login here'/>
-        </div>
-        <div class="form-group">
-          <label for="password">Password</label>
-          <input type="password" class="form-control" id="password" placeholder='enter your password here'/>
-        </div>
+  return (
+    <form>
+      <div className="form-group">
+        <label htmlFor="login">Login</label>
+        <input
+          type="text"
+          className="form-control"
+          id="login"
+          placeholder="enter your login here"
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="enter your password here"
+        />
+      </div>
 
-        <button type="button" class="btn btn-primary">Sign up</button>
-        <button type="button" class="btn btn-primary">Log in</button>
-      </form>  
-    );
+      <button type="button" className="btn btn-primary">
+        Sign up
+      </button>
+      <button type="button" className="btn btn-primary">
+        Log in
+      </button>
+    </form>
+  );
 };
 
 export default Form;
