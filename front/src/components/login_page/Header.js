e9fa60bdbc755d4923081bdc806fde98ef42f67e
Verteplushka <kolbacin_vlad@mail.ru>
2024-01-24 02:56:15 +0300

    Added hash to passwords

--- front/src/components/login_page/Header.js
+++ front/src/components/login_page/Header.js
@@ -4,7 +4,7 @@ const Header = ({ name, group, variant }) => {
   return (
     <header>
       <nav className="navbar navbar-dark bg-dark">
-        <h1 className="navbar-brand">
+        <h1 className="container navbar-brand text-center">
           {name}, {group}, Вариант №{variant}
         </h1>
       </nav>
